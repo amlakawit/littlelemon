0bb75cdc38289438ff3656a358403a4e0faa0608
amlakawit <40707808+amlakawit@users.noreply.github.com>
2024-01-07 00:48:29 -0500

    APIs secured

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework import permissions
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer, UserSrializer
 
@@ -19,6 +20,8 @@ class UserViewSet(viewsets.ModelViewSet):
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
+
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
@@ -28,3 +31,4 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
